ca6b370beed40f24f9ef475f4ad0e5ad61549a36
Michał Lemke <contact@michallemke.com>
2024-05-29 07:54:04 +0200

    Initial commit

---
+++ test/easy_one.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as EasyOne from '../lib/easy_one-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/easy_one-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new EasyOne.EasyOneStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
