a71a2d9238bdcbab232e70bf8638e3776088ae57
Michał Lemke <contact@michallemke.com>
2024-05-29 22:19:01 +0200

    initial

--- lib/easy_one-stack.ts
+++ lib/easy_one-stack.ts
@@ -1,16 +1,75 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import { custom_resources } from 'aws-cdk-lib';
 
-export class EasyOneStack extends cdk.Stack {
+export class MeraparLowHangingFruitStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // dynamodb
+    const table = new dynamodb.Table(this, 'MeraparLowHangingFruitDemoTable', {
+      partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'EasyOneQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // seed
+    new custom_resources.AwsCustomResource(this, 'MeraparLowHangingFruitDemoData', {
+      onCreate: {
+        service: 'DynamoDB',
+        action: 'putItem',
+        parameters: {
+          TableName: table.tableName,
+          Item: {
+            id: {S: 'value'},
+            text: { S: 'Hello, Merapar &#128522;' },
+          }
+        },
+        physicalResourceId: custom_resources.PhysicalResourceId.of(Date.now().toString()),
+      },
+      policy: custom_resources.AwsCustomResourcePolicy.fromSdkCalls({
+        resources: [table.tableArn],
+      }),
+    });
+
+    // lambda
+    const func = new lambda.Function(this, 'MeraparLowHangingFruitDemoFunction', {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromInline(`
+        const { DynamoDBClient, GetItemCommand } = require('@aws-sdk/client-dynamodb');
+        const client = new DynamoDBClient({});
+        exports.handler = async function(event) {
+          const params = {
+            TableName: process.env.TABLE_NAME,
+            Key: {
+              id: { S: 'value' },
+            },
+          };
+          const data = await client.send(new GetItemCommand(params));
+          const value = data.Item ? data.Item.text.S : 'No value found';
+          const html = \`<html><body><h1>The saved string is \${value}</h1></body></html>\`;
+          return {
+            statusCode: 200,
+            headers: { 'Content-Type': 'text/html' },
+            body: html,
+          };
+        };
+      `),
+      environment: {
+        TABLE_NAME: table.tableName,
+      },
+    });
+
+    table.grantReadData(func);
+
+    const api = new apigateway.LambdaRestApi(this, 'MeraparLowHangingFruitDemoApi', {
+      handler: func,
+    });
+
+    new cdk.CfnOutput(this, 'MeraparLowHangingFruitApiUrl', {
+      value: api.url,
+    });
   }
 }
--- test/easy_one.test.ts
+++ test/easy_one.test.ts
@@ -1,13 +1,13 @@
 // import * as cdk from 'aws-cdk-lib';
 // import { Template } from 'aws-cdk-lib/assertions';
-// import * as EasyOne from '../lib/easy_one-stack';
+// import * as LowHangingFruit from '../lib/easy_one-stack';
 
 // example test. To run these tests, uncomment this file along with the
 // example resource in lib/easy_one-stack.ts
 test('SQS Queue Created', () => {
 //   const app = new cdk.App();
 //     // WHEN
-//   const stack = new EasyOne.EasyOneStack(app, 'MyTestStack');
+//   const stack = new LowHangingFruit.MeraparLowHangingFruitStack(app, 'MyTestStack');
 //     // THEN
 //   const template = Template.fromStack(stack);
 
